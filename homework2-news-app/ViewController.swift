1b5284bda144feb686d2465837a6d9f69e6910e0
Samet Koyuncu <sametkoyuncu@live.com>
2022-09-11 21:16:05 +0300

    Add initial commit

---
+++ homework2-news-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homework2-news-app
+//
+//  Created by Samet Koyuncu on 11.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
