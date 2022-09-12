da16120302b476337c71c983ca50fdcf39c85c6b
Samet Koyuncu <sametkoyuncu@live.com>
2022-09-12 15:28:49 +0300

    Add categories view controller and appIcons

--- homework2-news-app/Controllers/NewsViewController.swift
+++ homework2-news-app/Controllers/NewsViewController.swift
@@ -9,19 +9,39 @@ import UIKit
 
 class NewsViewController: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
+    // when change selected category, reload news
+    var selectedCategory: Category? {
+        didSet{
+          loadNews()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // add notification center for getting selected category from the categories view controller
+        let notificationCenter: NotificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(self.changeCategory(_:)), name: .categoryNotification, object: nil)
         
         collectionView.delegate = self
         collectionView.dataSource = self
         
         registerCell()
-        
+    }
+    // notification center selector method for get and set selected category
+    @objc func changeCategory(_ notification: NSNotification) {
+        if let category = notification.userInfo?["category"] as? Category {
+          selectedCategory = category
+          }
+    }
+ 
+    // for register custom cell
+    func registerCell() {
+        collectionView.register(.init(nibName: K.Cell.newsCellNibName, bundle: nil), forCellWithReuseIdentifier: K.Cell.newsCellId)
     }
     
-    func registerCell() {
-        collectionView.register(.init(nibName: K.newsCellNibName, bundle: nil), forCellWithReuseIdentifier: K.newsCellId)
+    // load news when selected category changed
+    func loadNews() {
+        print(selectedCategory?.name)
     }
 }
 
@@ -33,9 +53,9 @@ extension NewsViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 10
     }
-    
+    // set cell data
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: K.newsCellId, for: indexPath) as! NewsCell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: K.Cell.newsCellId, for: indexPath) as! NewsCell
         
         cell.titleLabel.text = "cell for \(indexPath.row)"
         cell.image.image = UIImage(named: "hamburgerMenu")
@@ -43,7 +63,7 @@ extension NewsViewController: UICollectionViewDataSource {
         return cell
     }
 }
-
+// set cell width using device sizes
 extension NewsViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let width = collectionView.frame.width / 2 - 15
